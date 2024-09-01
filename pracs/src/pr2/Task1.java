15000a3e8c6927b878a45dca0d14d58f42399071
p0pl3 <m.poplavskiy04@gmail.com>
2024-09-02 01:13:06 +0300

    fix pr2 task1

--- pracs/src/pr2/Task1.java
+++ pracs/src/pr2/Task1.java
@@ -1,16 +1,22 @@
 package pr2;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Task1 {
     public static void main(String[] args) {
-        try (FileInputStream fin = new FileInputStream("E:/study/7sem/rksp//praccks/pracs/src/pr2/task1.txt")) {
-            int i = -1;
-            while ((i = fin.read()) != -1) {
-                System.out.print((char) i);
+        String filePath = "src/pr2/task1.txt";
+        // Читаем содержимое файла и выводим его
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+            System.out.println("Содержимое файла:");
+            for (String line : lines) {
+                System.out.println(line);
             }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
         }
     }
 }
